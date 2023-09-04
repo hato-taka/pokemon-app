5c630b60b8afeeae83cc19e9f17939e23962be73
hato-taka <84255881+hato-taka@users.noreply.github.com>
2023-09-04 20:35:34 +0900

    [add] propsを渡す

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 import PokemonThumbnails from './PokemonThumbnails';
 
 function App() {
+
+  // 仮でデータを作成する
+  const pokemon = {
+    id: 1,
+    name: "フシギダネ",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    type: "くさ"
+  }
+
   return (
     <div className="App">
-      <PokemonThumbnails />
+      <PokemonThumbnails
+        id={pokemon.id}
+        name={pokemon.name}
+        image={pokemon.image}
+        type={pokemon.type} />
     </div>
   );
 }
--- src/PokemonThumbnails.jsx
+++ src/PokemonThumbnails.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const PokemonThumbnails = () => {
-  return <div>PokemonThumbnails</div>;
+const PokemonThumbnails = ({ id, name, image, type }) => {
+  return (
+    <div>
+      <div className="number">
+        <small>#0{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <div className="detail-wrapper">
+        <h4>{name}</h4>
+        <h3>{type}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default PokemonThumbnails;
