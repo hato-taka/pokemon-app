7a51ccf357a5f204ef9e4b7afe3af81932b80ce3
hato-taka <84255881+hato-taka@users.noreply.github.com>
2023-09-05 18:28:41 +0900

    [fix] map()関数で繰り返し処理

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [pokemonNames, setPokemonNames] = useState([]);
 
   // 仮でデータを作成する
-  const pokemon = [
+  // 複数形pokemons に修正
+  const pokemons = [
     {
       id: 1,
       image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
@@ -32,14 +33,14 @@ function App() {
     fetch(url)
       .then(res => res.json())
       .then(data => {
-        console.log(data.results[0].name);
         // 仮で３つのポケモンの名前をセットする
-        const name = [
+        // 複数形に修正
+        const names = [
           data.results[0].name,
           data.results[1].name,
           data.results[2].name,
         ]
-        setPokemonNames(name);
+        setPokemonNames(names);
       })
   }, [])
 
@@ -48,21 +49,15 @@ function App() {
       <h1>ポケモン図鑑</h1>
       <div className='pokemon-container'>
         <div className='all-container'>
-          <PokemonThumbnails
-            id={pokemon[0].id}
-            name={pokemonNames[0]}
-            image={pokemon[0].image}
-            type={pokemon[0].type} />
-          <PokemonThumbnails
-            id={pokemon[1].id}
-            name={pokemonNames[1]}
-            image={pokemon[1].image}
-            type={pokemon[1].type} />
-          <PokemonThumbnails
-            id={pokemon[2].id}
-            name={pokemonNames[2]}
-            image={pokemon[2].image}
-            type={pokemon[2].type} />
+          {pokemons.map((pokemon, index) => (
+            <PokemonThumbnails
+              id={pokemon.id}
+              name={pokemonNames[index]}
+              image={pokemon.image}
+              type={pokemon.type}
+              key={index}
+            />
+          ))}
         </div>
       </div>
     </div>
